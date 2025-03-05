e48181c7f50591d577aaa73f4c62442c5b54866d
Shivani Madhan <madhan.shivani1104@gmail.com>
2025-03-05 15:09:15 -0800

    changed backend url and port

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ initChatSocket(io);
 initVotingSocket(io); 
 
 // Start the Server
-const PORT = 5001;
+const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
